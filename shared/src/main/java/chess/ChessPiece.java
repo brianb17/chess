87891b4e47fcfa3b1df2eded96b0395f5fc77b81
brianb17 <babaak@byu.edu>
2025-09-18 01:36:48 -0600

    Bishop, Rook, Queen, and Knight all finally working.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -62,6 +62,21 @@ public class ChessPiece {
                 {1,1}, {1,-1}, {-1,1}, {-1,-1}
             });
         }
+        if (piece.getPieceType() == PieceType.ROOK) {
+            return slidingMoves(board, myPosition, piece, new int[][] {
+                    {1,0}, {-1,0}, {0,1}, {0,-1}
+            });
+        }
+        if (piece.getPieceType() == PieceType.QUEEN) {
+            return slidingMoves(board, myPosition, piece, new int[][] {
+                    {1,1}, {1,-1}, {-1,1}, {-1,-1}, {1,0}, {-1,0}, {0,1}, {0,-1}
+            });
+        }
+        if (piece.getPieceType() == PieceType.KNIGHT) {
+            return jumpMoves(board, myPosition, piece, new int[][] {
+                    {2,1}, {1,2}, {-1,2}, {-2,1}, {-2,-1}, {-1,-2}, {1,-2}, {2,-1}
+            });
+        }
         return List.of();
     }
 
@@ -91,6 +106,29 @@ public class ChessPiece {
                         moves.add(new ChessMove(start, newPos, null));
                     }
                     break;
+                }
+            }
+        }
+        return moves;
+    }
+
+    private Collection<ChessMove> jumpMoves(
+            ChessBoard board, ChessPosition start, ChessPiece piece, int[][] directions) {
+
+        List<ChessMove> moves = new ArrayList<>();
+        int startRow = start.getRow();
+        int startCol = start.getColumn();
+
+        for (int[] offset : directions) {
+            int newRow = startRow + offset[0];
+            int newCol = startCol + offset[1];
+
+            if (newRow >= 1 && newRow <= 8 && newCol >= 1 && newCol <= 8) {
+                ChessPosition newPos = new ChessPosition(newRow, newCol);
+                ChessPiece target = board.getPiece(newPos);
+
+                if (target == null || target.getTeamColor() != piece.getTeamColor()) {
+                    moves.add(new ChessMove(start, newPos, null));
                 }
             }
         }
