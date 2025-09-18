7090bed58376e390b8135280c58de44815b1b076
brianb17 <babaak@byu.edu>
2025-09-18 02:38:34 -0600

    Pawn working now. Added pawnMoves helper and addPawnMove helper.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -82,6 +82,9 @@ public class ChessPiece {
                 {1,1}, {1,-1}, {-1,1}, {-1,-1}, {1,0}, {-1,0}, {0,1}, {0,-1}
             });
         }
+        if (piece.getPieceType() == PieceType.PAWN) {
+            return pawnMoves(board, myPosition, piece);
+        }
         return List.of();
     }
 
@@ -138,6 +141,76 @@ public class ChessPiece {
             }
         }
         return moves;
+    }
+
+    private Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition start, ChessPiece piece) {
+        List<ChessMove> moves = new ArrayList<>();
+
+        int startRow = start.getRow();
+        int startCol = start.getColumn();
+        int direction;
+        if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+            direction = 1;
+        }
+        else {
+            direction = -1;
+        }
+        int frontRow = startRow + direction;
+        if (frontRow >= 1 && frontRow <= 8) {
+            ChessPosition forwardPos = new ChessPosition(frontRow, startCol);
+            if (board.getPiece(forwardPos) == null) {
+                addPawnMove(moves, start, forwardPos, piece);
+
+                int sRank;
+                if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                    sRank = 2;
+                }
+                else {
+                    sRank = 7;
+                }
+                if (startRow == sRank) {
+                    int doubleRow = startRow + 2 * direction;
+                    ChessPosition doublePos = new ChessPosition(doubleRow, startCol);
+                    if (board.getPiece(doublePos) == null) {
+                        moves.add(new ChessMove(start, doublePos, null));
+                    }
+                }
+            }
+        }
+
+        int[][] captures = { {direction, -1}, {direction, 1} };
+        for (int[] cap : captures) {
+            int newRow = startRow + cap[0];
+            int newCol = startCol + cap[1];
+
+            if (newRow >= 1 && newRow <= 8 && newCol >= 1 && newCol <= 8) {
+                ChessPosition capturePos = new ChessPosition(newRow, newCol);
+                ChessPiece target = board.getPiece(capturePos);
+                if (target != null && target.getTeamColor() != piece.getTeamColor()) {
+                    addPawnMove(moves, start, capturePos, piece);
+                }
+            }
+        }
+        return moves;
+    }
+
+    private void addPawnMove(List<ChessMove> moves, ChessPosition start, ChessPosition end, ChessPiece piece) {
+        int promoRow;
+        if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+            promoRow = 8;
+        }
+        else {
+            promoRow = 1;
+        }
+        if (end.getRow() == promoRow) {
+            moves.add(new ChessMove(start, end, ChessPiece.PieceType.QUEEN));
+            moves.add(new ChessMove(start, end, ChessPiece.PieceType.BISHOP));
+            moves.add(new ChessMove(start, end, ChessPiece.PieceType.ROOK));
+            moves.add(new ChessMove(start, end, ChessPiece.PieceType.KNIGHT));
+        }
+        else {
+            moves.add(new ChessMove(start, end, null));
+        }
     }
 
 
