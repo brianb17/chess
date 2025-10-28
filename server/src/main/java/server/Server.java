21fd0cf9661f59b7b5d1e2e5cd4be110c96657a9
brianb17 <babaak@byu.edu>
2025-10-27 18:44:04 -0600

    javlin should be working(phase 3)

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,49 @@
+package server;
+
+import com.google.gson.Gson;
+import dataaccess.MemoryDataAccess;
+import datamodel.UserData;
+import io.javalin.*;
+import io.javalin.http.Context;
+import service.UserService;
+
+public class Server {
+
+    private final Javalin server;
+    private final UserService userService;
+
+    public Server() {
+        var dataAccess = new MemoryDataAccess();
+        userService = new UserService(dataAccess);
+        server = Javalin.create(config -> config.staticFiles.add("web"));
+
+        server.delete("db", ctx -> ctx.result("{}"));
+        server.post("user", this::register);
+        // Register your endpoints and exception handlers here.
+
+    }
+
+    private void register(Context ctx) {
+        try {
+            var serializer = new Gson();
+            String reqJson = ctx.body();
+            var user = serializer.fromJson(reqJson, UserData.class);
+
+            var authData = userService.register(user);
+
+            ctx.result(serializer.toJson(authData));
+        } catch (Exception ex) {
+            var msg = String.format("{\"message\": \"Error: %s\"}", ex.getMessage());
+            ctx.status(403).result();
+        }
+    }
+
+    public int run(int desiredPort) {
+        server.start(desiredPort);
+        return server.port();
+    }
+
+    public void stop() {
+        server.stop();
+    }
+}
---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,28 @@
+package service;
+
+import dataaccess.DataAccess;
+import datamodel.AuthData;
+import datamodel.UserData;
+
+public class UserService {
+
+    private final DataAccess dataAccess;
+
+    public UserService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public AuthData register(UserData user) throws Exception {
+        if (dataAccess.getUser(user.username()) != null) {
+            throw new Exception("already exists");
+        }
+        dataAccess.createUser(user);
+        var authData = new AuthData(user.username(), generateAuthToken());
+
+        return authData;
+    }
+
+    private String generateAuthToken() {
+        return "xyz";
+    }
+}
