7c5e85729b967cb9d0992b754ee340df48794fe9
brianb17 <babaak@byu.edu>
2025-11-04 17:10:00 -0700

    Addded Auth and Game to DataAccess(phase 3)

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -2,8 +2,19 @@ package dataaccess;
 
 import datamodel.*;
 
+import java.util.Collection;
+import java.util.HashMap;
+
 public interface DataAccess {
     void clear();
     void createUser(UserData user);
     UserData getUser(String username);
+
+    void createAuth(AuthData auth);
+    AuthData getAuth(String token);
+    void deleteAuth(String token);
+
+    void createGame(GameData game);
+    GameData getGame(int gameID);
+    HashMap<Integer, GameData> getAllGames();
 }
--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -9,9 +9,13 @@ import java.util.HashSet;
 
 public class MemoryDataAccess implements DataAccess{
     private final HashMap<String, UserData> users = new HashMap<>();
+    private final HashMap<String, AuthData> authTokens = new HashMap<>();
+    private final HashMap<Integer, GameData> games = new HashMap<>();
     @Override
     public void clear() {
         users.clear();
+        authTokens.clear();
+        games.clear();
     }
 
     @Override
@@ -22,5 +26,29 @@ public class MemoryDataAccess implements DataAccess{
     @Override
     public UserData getUser(String username) {
         return users.get(username);
+    }
+
+    public void createAuth(AuthData auth) {
+        authTokens.put(auth.authToken(), auth);
+    }
+    public AuthData getAuth(String token) {
+        return authTokens.get(token);
+    }
+
+    public void deleteAuth(String token) {
+        authTokens.remove(token);
+    }
+
+    public void createGame(GameData game) {
+        games.put(game.gameID(), game);
+    }
+
+    public GameData getGame(int id) {
+        return games.get(id);
+
+    }
+
+    public HashMap<Integer, GameData> getAllGames() {
+        return games;
     }
 }
