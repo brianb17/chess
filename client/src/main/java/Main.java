059ff987364590d8ae1f0aed43f8a4f4a4dc936e
brianb17 <babaak@byu.edu>
2025-11-26 23:59:10 -0700

    fixed client calling server (phase5)

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -5,8 +5,7 @@ import ui.PreloginUI;
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server();
-        int port = server.run(8080);
+        int port = 8080;
         System.out.println("Server running on port " + port);
 
         ServerFacade facade = new ServerFacade(port);
